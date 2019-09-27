7945a07aad8e35e8a0dc99f9ac0c12641ba88964
Artyom Lezgyan <33369719+Paleontolog@users.noreply.github.com>
2019-09-27 18:52:53 +0400

    Update server.py

--- server.py
+++ server.py
@@ -40,6 +40,5 @@ def get_miner():
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
-    # app.debug = True  # enables auto reload during development
     # app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
     # app.run(host='192.168.0.120', port=8080)
