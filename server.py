5e2e54df67a5ca28d092a10e9dd5f034410b71ee
Artyom Lezgyan <33369719+Paleontolog@users.noreply.github.com>
2019-09-27 19:42:31 +0400

    Update server.py

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 import flask
 import json
-import os
+#import os
 # import decoder
 from flask import render_template
 
@@ -46,8 +46,8 @@ def get_miner():
 
 
 if __name__ == '__main__':
-   # app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
-   # port = int(os.environ.get('PORT', 5000))
-    #app.run(host='0.0.0.0', port=port)
-    app.run()
+    app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+    #app.run()
     # app.run(host='192.168.0.120', port=8080)
