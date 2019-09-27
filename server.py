510a0fb078f544d4b80d84dc800026492bd002c3
Artyom Lezgyan <33369719+Paleontolog@users.noreply.github.com>
2019-09-27 19:50:34 +0400

    Update server.py

--- server.py
+++ server.py
@@ -34,7 +34,7 @@ def get_str2():
     
 @app.route('/miner', methods=['GET'])
 def get_miner():
-    return render_template("miner12.html")
+    return render_template("miner.html")
 
 
 if __name__ == '__main__':
